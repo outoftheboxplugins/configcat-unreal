faad386e39315f28c9d8420f04b10d49561e8cf7
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-11-15 22:44:24 +0200

    trying CMAKE_CXX_EXTENSIONS

--- Extras/triplets/arm64-linux-unreal.cmake
+++ Extras/triplets/arm64-linux-unreal.cmake
@@ -2,8 +2,8 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CXX_STANDARD=98 -DCMAKE_CXX_EXTENSIONS=OFF")
 
-set(VCPKG_CXX_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++98")
-set(VCPKG_C_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++98")
-set(VCPKG_LINKER_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++98")
+set(VCPKG_CXX_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics")
+set(VCPKG_C_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics")
+set(VCPKG_LINKER_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics")
