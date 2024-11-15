fa6044588047754eef372961e78ea299685521c4
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-11-15 22:33:10 +0200

    older standard

--- Extras/triplets/arm64-linux-unreal.cmake
+++ Extras/triplets/arm64-linux-unreal.cmake
@@ -4,6 +4,6 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-set(VCPKG_CXX_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++11")
-set(VCPKG_C_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++11")
-set(VCPKG_LINKER_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++11")
+set(VCPKG_CXX_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++98")
+set(VCPKG_C_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++98")
+set(VCPKG_LINKER_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics -std=c++98")
