5e776bb763ece44e152522fef60b792689d03b86
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-11-15 12:55:02 +0200

    trying C standard instead of CXX

--- Extras/triplets/arm64-linux-unreal.cmake
+++ Extras/triplets/arm64-linux-unreal.cmake
@@ -4,7 +4,7 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CXX_STANDARD=17")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_C_STANDARD=17")
 
 set(VCPKG_CXX_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics")
 set(VCPKG_C_FLAGS_RELEASE "-stdlib=libc++ -mno-outline-atomics")
